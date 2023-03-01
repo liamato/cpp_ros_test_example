96fe90f5fcc01187cc69ed85cc216f2c3ec573c1
Marc Duran Olivé <liamato97@gmail.com>
2023-03-01 22:14:32 +0100

    Added a base for unit testing for the Counter class

---
+++ test/counter_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "cpp_ros_test_example/ExampleLib.h"
+
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
