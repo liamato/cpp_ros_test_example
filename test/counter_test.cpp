942a0907cf50251a74256d04515e5ff3a7b0b4c2
Marc Duran Olivé <liamato97@gmail.com>
2023-03-02 00:03:18 +0100

    Implemented some unit tests for the Counter class

--- test/counter_test.cpp
+++ test/counter_test.cpp
@@ -2,6 +2,34 @@
 
 #include "cpp_ros_test_example/ExampleLib.h"
 
+using cpp_ros_test_example::Counter;
+
+TEST(Counter, defaultConstructor) {
+    Counter c;
+    EXPECT_EQ(c.get(), 0);
+}
+
+TEST(Counter, next) {
+    Counter c;
+    c.next();
+    EXPECT_EQ(c.get(), 1);
+    c.next();
+    EXPECT_EQ(c.get(), 2);
+    c.next();
+    c.next();
+    EXPECT_EQ(c.get(), 4);
+}
+
+TEST(Counter, previous) {
+    Counter c;
+    c.previous();
+    EXPECT_EQ(c.get(), -1);
+    c.previous();
+    EXPECT_EQ(c.get(), -2);
+    c.previous();
+    c.previous();
+    EXPECT_EQ(c.get(), -4);
+}
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
